07a48800b16183fe9046052d3be78288136bcd39
gahee <sarahan774@gmail.com>
2020-05-04 18:19:31 +0900

    update database operation code

---
+++ database_models.py
@@ -0,0 +1,42 @@
+from peewee import *
+import credentials
+
+
+db = SqliteDatabase('pet_hotels.db')
+sqlite_db = MySQLDatabase('my_petmily', user='root', password=credentials.MYSQL_PASSWORD,
+                          host='localhost', port=3306)
+
+
+class Hotel(Model):
+    id = BigAutoField(primary_key=True)
+    name = CharField(max_length=50, null=True, unique=True)
+    address = CharField(max_length=255, null=True, unique=True)
+    address_detail = CharField(max_length=255, null=True)
+    zipcode = IntegerField(default=0, null=True)
+    latitude = FloatField(default=0, null=True)
+    longitude = FloatField(default=0, null=True)
+    week_open_time = DateTimeField(null=True)
+    week_close_time = DateTimeField(null=True)
+    sat_open_time = DateTimeField(null=True)
+    sat_close_time = DateTimeField(null=True)
+    sun_open_time = DateTimeField(null=True)
+    sun_close_time = DateTimeField(null=True)
+    week_price = IntegerField(null=True)
+    sat_price = IntegerField(null=True)
+    sun_price = IntegerField(null=True)
+    phone_number = CharField(null=True)
+    is_monitored = BooleanField(null=True)
+    is_neutered_only = BooleanField(null=True)
+    supported_dog_size = IntegerField(null=True)
+    user_rating = FloatField(null=True)
+    star = IntegerField(null=True)
+    created_at = DateTimeField(null=True)
+    updated_at = DateTimeField(null=True)
+
+    class Meta:
+        database = sqlite_db
+        table_name = 'hotels'
+        indexes = (
+            (('name', 'address'), True)
+        )
+
--- naver_maps.py
+++ naver_maps.py
@@ -1,12 +1,9 @@
 import urllib.request
-import time
 import json
-import pymysql
-import sqlite3
 import datetime
+from database_models import Hotel, sqlite_db
+import credentials
 
-CLIENT_ID = "ubtH0XiUQUZeoZfZllfX"
-CLIENT_SECRET = "Ag2zHxP5gc"
 
 
 seoul_gu_names = ["남구", "강동구","강북구", "강서구", "관악구","광진구",
@@ -25,8 +22,8 @@ def search_places(query, display=30, start=1, sort="random"):
           "&sort=" + sort
     # print(url)
     request = urllib.request.Request(url)
-    request.add_header("X-Naver-Client-Id", CLIENT_ID)
-    request.add_header("X-Naver-Client-Secret", CLIENT_SECRET)
+    request.add_header("X-Naver-Client-Id", credentials.CLIENT_ID)
+    request.add_header("X-Naver-Client-Secret", credentials.CLIENT_SECRET)
     response = urllib.request.urlopen(request)
 
     response_code = response.getcode()
@@ -38,7 +35,7 @@ def search_places(query, display=30, start=1, sort="random"):
         # 결과를 JSON 키값으로 접근하기 위해 json.loads 를 한다.
         result = json.loads(decoded_body)
         print(result)
-        # save_to_database(result)
+        save_to_database_server(result)
 
         # 결과가 30개 이상이면 start 를 30 더해서 다시 검색한다.
         if len(result["items"]) >= display:
@@ -48,36 +45,21 @@ def search_places(query, display=30, start=1, sort="random"):
         print("Error Code:" + response_code)
 
 
-def save_to_database(results):
-    hotels_db = pymysql.connect(
-        user='root',
-        passwd='rkgml8590',
-        host='127.0.0.1',
-        port=4000,
-        db='mydatabase',
-        charset='utf8'
-    )
-
-    cursor = hotels_db.cursor(pymysql.cursors.DictCursor)
-    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+def save_to_database_server(results):
+    sqlite_db.connect(reuse_if_open=True)
+    timestamp = datetime.datetime.now()
 
     for item in results["items"]:
-        sql = '''
-            INSERT INTO
-            hotels(name, address, latitude, longitude, phone_number, created_at)
-            VALUES({name}, {address}, {latitude}, {longitude}, {phone_number}, {created_at})
-            '''.format(name=item["title"], address=item["roadAddress"],
-                       latitude=item["mapx"], longitude=item["mapy"],
-                       phone_number=item["telephone"], created_at=timestamp)
+        query = Hotel.insert(name=item["title"], address=item["roadAddress"],
+                      latitude=float(item["mapx"]), longitude=float(item["mapy"]),
+                      phone_number=item["telephone"], created_at=timestamp)
+
         try:
-            cursor.execute(sql)
-            print("executed")
-            hotels_db.commit()
-            rows = cursor.fetchall()
-            print(rows)
-        except:
-            hotels_db.rollback()
-            print("failed")
+            query.on_conflict(action='REPLACE')
+            query.execute()
+        except Exception as e:
+            print(e)
+            sqlite_db.rollback()
 
 
 if __name__ == '__main__':
@@ -86,3 +68,4 @@ if __name__ == '__main__':
         # time.sleep(0.5)
         print("==== RESULT OF " + name + " ====")
         search_places(query=name + " 애견호텔")
+
